931b5ff3451f0e88cc1c9c6be243b2b246ffe869
gabiavila4 <123492198+gabiavila4@users.noreply.github.com>
2025-09-30 10:11:48 -0500

    9/30

--- foldActivity/main.cpp
+++ foldActivity/main.cpp
@@ -6,9 +6,52 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int add(int a, int b){
+    return  a + b;
+}
+
+int fold(vector<int>& vec, function <int(int, int)> func)
+{
+//   static int result;
+//    static int i;
+//    if (i < vec.size()){
+//        int r1 = vec[i];
+//        int r2 = vec[i+1];
+//        vec = {};
+//        for (int& in : vec)
+//        {
+//            vec.push_back(vec[i + in]);
+//        }
+//        return result = func(r1,r2) + fold(vec, func);
+//    }else
+//    {
+//        return result;
+//    }
+//    
+//    int result = vec[0]
+//    for int i = 0; i< vec
+    
+    if (vec.empty())
+    {
+        return 0;
+    }
+    int result = vec[0];
+    for (size_t i = 1; i < vec.size(); i++)
+    {
+        result = func(result, vec[i]);
+    }
+    return result;
+    
+}
+
+int main() {
+   
+    vector <int> vec = {4, 3, 2, 1};
+    function <int(int, int)> func;
+    func = add;
+    int hi = fold(vec, func);
+    cout << hi;
 }
